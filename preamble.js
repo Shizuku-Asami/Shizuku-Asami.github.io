e5f722e0cafb5f964f9d1eb3ab4656e91bd86523
Shizuku-Asami <xv.chronos@gmail.com>
2021-04-23 20:31:46 +0300

    norm notation

--- preamble.js
+++ preamble.js
@@ -5,6 +5,7 @@ window.MathJax = {
         macros: {
             vect: ["\\vec{\\bf{#1}}", 1],
             uvect: ["\\hat{\\bf{#1}}", 1],
+            norm: ["\\left\\Vert#1\\right\\Vert", 1],
             abs: ["\\left|#1\\right|", 1]
         }
     }
